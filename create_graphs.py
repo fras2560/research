a861d2bcc81dbeda9aade6aa61f52072ade6a8d7
fras2560 <dallas_j_frase@hotmail.com>
2014-10-21 09:12:41 -0400

    Prints the total completed percentage

--- create_graphs.py
+++ create_graphs.py
@@ -14,7 +14,7 @@ Version: 2014-10-19
 from graph.helper import make_claw, make_cycle, make_cok4
 from os import getcwd
 from os.path import join
-from generator import Generator
+from generator import Generator 
 from file import File
 import logging
 
@@ -28,13 +28,19 @@ cok4 = make_cok4()
 FAMILY = "ClawC4CoK4"
 DIRECTORY = join(getcwd(), 'GraphFamilies', FAMILY)
 FORBIDDEN = [claw, c4, cok4]
-STARTING = make_cycle(5)
+STARTING = make_cycle(7)
 logging.basicConfig(filename=FAMILY+".log", level=logging.INFO,
                             format='%(asctime)s %(message)s')
 LOGGER = logging.getLogger(__name__)
-# processing work
-generator = Generator(STARTING, 14, FORBIDDEN)
+# processing work 
+generator = Generator(STARTING, 5, FORBIDDEN)
+total = generator.total_graphs()
+print("Total Graphs: %d" % total)
+index = 0
 for graph in generator.iterate():
+    if index % 100 == 0:
+        print("Percentage :{0:.2f}".format(index / total))
+    index += 1
     f = File(DIRECTORY, G=graph, logger=LOGGER)
     f.save()
 LOGGER.info("Complete")
