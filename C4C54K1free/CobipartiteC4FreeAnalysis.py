f13097544e07b5688937842a803cbcd737f6cfc0
Tom LaMantia <lama3790@mylaurier.ca>
2015-08-05 14:47:35 -0400

    Checking that both cliques contain only comparable vertices!

--- C4C54K1free/CobipartiteC4FreeAnalysis.py
+++ C4C54K1free/CobipartiteC4FreeAnalysis.py
@@ -1,4 +1,5 @@
 import sys
+from networkx.classes.function import neighbors
 sys.path.append("..") # Adds higher directory to python modules path
 from itertools import product, combinations
 import networkx as nx
@@ -9,7 +10,7 @@ from http.client import FORBIDDEN
 
 FORBIDDEN_SUBGRAPHS = {make_cycle(4)}
 
-def make_clique(n, vertexStartIndex):
+def MakeClique(n, vertexStartIndex):
     '''
     makes a clique of size n
     Parameters:
@@ -36,7 +37,7 @@ def GIsHFree(G, H):
             break
     return result
 
-def calculateGraphDensity(G):
+def CalculateGraphDensity(G):
     
     E = len(G.edges())
     V = len(G.nodes())
@@ -48,12 +49,35 @@ def calculateGraphDensity(G):
     
     return result
 
-def generateAllCobipartiteGraphs(c1Size, c2Size):
+def IsComparable(G, vertices):
+    
+    result = True
+    
+    for v in vertices:
+        for u in vertices:
+            if u != v:
+                closedNeighbourhoodU = G.neighbors(u)
+                closedNeighbourhoodV = G.neighbors(v)
+                #We are only interested neighbors in the other clique!
+                for thisVertex in vertices:
+                    if thisVertex in closedNeighbourhoodU:
+                        closedNeighbourhoodU.remove(thisVertex)
+                    if thisVertex in closedNeighbourhoodV:
+                        closedNeighbourhoodV.remove(thisVertex)
+                
+                if closedNeighbourhoodU == closedNeighbourhoodV:
+                    result = False
+                    break
+        if result == False:
+            break
+        
+    return result
 
-    c1 = make_clique(c1Size,0)
-    c2 = make_clique(c2Size, c1Size)
+def GenerateAllCobipartiteGraphs(c1Size, c2Size):
 
-    #allEdgeConfigurations = product(c1.nodes(), c2.nodes())
+    c1 = MakeClique(c1Size,0)
+    c2 = MakeClique(c2Size, c1Size)
+
     for size in range(0, (c1Size*c2Size) + 1):
         for thisEdgeSet in combinations(product(c1.nodes(), c2.nodes()), size):
             
@@ -63,11 +87,13 @@ def generateAllCobipartiteGraphs(c1Size, c2Size):
                 graphToTest.add_edge(thisEdge[0], thisEdge[1])
                 
             if GIsHFree(graphToTest, FORBIDDEN_SUBGRAPHS):
-                print(thisEdgeSet)
-                thisGraphDensity = calculateGraphDensity(graphToTest)
-                print("Density: {0}".format(thisGraphDensity))
+                
+                #Check to make sure every vertex in c1 and c2 is comparable
+                if IsComparable(graphToTest, c1) == True and IsComparable(graphToTest, c2) == True:
+                    print({i for i in thisEdgeSet if (i not in c1) and (i not in c2)})
+                    #thisGraphDensity = CalculateGraphDensity(graphToTest)
+                    #print("Density: {0}".format(thisGraphDensity))
             graphToTest.clear()
-
     return
 
-generateAllCobipartiteGraphs(3, 3)
+GenerateAllCobipartiteGraphs(3, 3)
